b7df7742cbc4b9acd9ee3b1dbff5cde47621fc8e
huzaifahfa <hxf5155@psu.edu>
2026-04-14 14:28:56 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
